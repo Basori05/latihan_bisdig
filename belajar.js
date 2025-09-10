a7e904dfe420761c22f391d1db3ebdff355042e6
Muh.Basori Albagiah <e42242120@student.polije.ac.id>
2025-09-10 15:41:19 +0700

    javascript

---
+++ belajar.js
@@ -0,0 +1,32 @@
+function jalankanNative() {
+    alert("Ini adalah contoh alert()");
+
+    var yakin = confirm("Apakah kamu yakin ingin melanjutkan?");
+    if (yakin) {
+        alert("Kamu menekan OK di confirm()");
+    } else {
+        alert("Kamu menekan Cancel di confirm()");
+    }
+
+    var nama = prompt("Masukkan namamu:");
+    alert("Halo " + nama + "! ini hasil dari prompt()");
+
+    var now = new Date();
+    alert("Sekarang adalah: " + now);
+
+    var angka = parseInt("123");
+    alert("parseInt('123') menghasilkan angka: " + angka);
+
+    setTimeout(function () {
+        alert("Popup ini muncul setelah 3 detik (setTimeout)");
+    }, 3000);
+}
+
+// Custom functions
+function foo() {
+    alert("Custom function foo() baru saja dijalankan!");
+}
+
+function alertArraySize(arr) {
+    alert("Ukuran array adalah: " + arr.length);
+}
